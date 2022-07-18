02ba47c451c420cac924ce41e6d5aa9d9a4c2734
ebukanwuba45@gmail.com <ebukanwuba45@gmail.com>
2022-07-18 13:40:36 +0100

    intial-comit

---
+++ index.js
@@ -0,0 +1,31 @@
+const display =  document.getElementsByClassName("display")[0];
+display.style.fontSize = "30px";
+const buttons = Array.from(document.getElementsByClassName("button-content"));
+buttons.map(button =>{
+    button.addEventListener("click", (e) =>{
+        try {
+            switch(e.target.innerText){
+                case "CE" :
+                    if(e.target.innerText){
+                        display.innerText = '';
+                        break;
+                    }
+                    case "=" :
+                         display.innerText = eval(display.innerText);
+                        break;
+                default: 
+                display.innerText += e.target.innerText;
+            }
+        } 
+        catch{
+           display.innerText = "syntax error";
+        }
+      
+    })
+
+
+}) 
+  
+
+
+
